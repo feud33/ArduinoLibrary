a59fde8c051c864c143b597ecc75ef98a72c5106
unknown <FEUD7506@EB-OR7003102.bordeaux.francetelecom.fr>
2014-04-21 00:02:52 +0200

    getter/setter added to second, minutes...

--- Model/Calendar.h
+++ Model/Calendar.h
@@ -21,6 +21,19 @@ class Calendar {
   	void set(const byte second, const byte minute, const byte hour, const byte day, const byte month, const byte year);
   	void get(byte *second, byte *minute, byte *hour, byte *weekDay, byte *day, byte *month, byte *year) const;
   	void get(const int timeZone, const bool summerTime, byte *second, byte *minute, byte *hour, byte *weekDay, byte *day, byte *month, byte *year) const;
+  	byte getSecond() const;
+  	byte getMinute() const;
+  	byte getHour() const;
+  	byte getWeekDay() const;
+  	byte getDay() const;
+  	byte getMonth() const;
+  	byte getYear() const;
+  	void setSecond(byte second);
+  	void setMinute(byte minute);
+  	void setHour(byte hour);
+  	void setDay(byte day);
+  	void setMonth(byte month);
+  	void setYear(byte year);
   	char *toString(char *buffer) const;			// logger, 1 is sunday
 
   	void addSecond(const long second);			// to add seconds to the current object, can be greater than 60 and <0
